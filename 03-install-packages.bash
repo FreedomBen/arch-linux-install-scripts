7a537915ff4b198d854fec0763ac7c16bc2fedf8
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-08-16 18:15:37 -0600

    Add screen to installed packages

--- 03-install-packages.bash
+++ 03-install-packages.bash
@@ -161,6 +161,7 @@ pacman -S --noconfirm --needed avahi
 pacman -S --noconfirm --needed nss-mdns
 pacman -S --noconfirm --needed openssh
 pacman -S --noconfirm --needed dnsutils
+pacman -S --noconfirm --needed screen
 
 pacman -S --noconfirm --needed pkgfile
 pkgfile --update
