81e45382408628d8c8a5f972c2ef97396c73a678
Benjamin Porter <bporter@instructure.com>
2015-05-21 10:40:13 -0600

    Add xarchiver to stuffz

--- 03-install-packages.bash
+++ 03-install-packages.bash
@@ -239,6 +239,7 @@ if [ "$GNOME" = "Y" -o "$GNOME" = "y" ]; then
     pacman -S --noconfirm --needed gst-plugins-ugly
     pacman -S --noconfirm --needed gst-libav
     pacman -S --noconfirm --needed file-roller
+    pacman -S --noconfirm --needed xarchiver
     pacman -S --noconfirm --needed vino vinagre # VLC server and remote viewer Gnome style
     pacman -S --noconfirm --needed ttf-liberation # For google-chrome pdf rendering
 
